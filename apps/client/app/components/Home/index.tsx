dc0c31f4e05f0bdb4c46a0c47004962c4a2710d4
Walter <walter@sentbe.com>
2025-01-13 16:20:01 +0900

    Date 추가 / 마크다운 ui 변경

--- apps/client/app/components/Home/index.tsx
+++ apps/client/app/components/Home/index.tsx
@@ -1,8 +1,8 @@
 import type { LinksFunction } from "@remix-run/node";
 import type { PostMeta } from "~/types";
 import stylesUrl from "./styles.css";
-import dayjs from "dayjs";
 import { Link } from "@remix-run/react";
+import { formatDate } from "~/utils/date";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -15,7 +15,7 @@ export const Home: React.FC<Props> = ({ posts }) => {
   return (
     <section className="post_list">
       {posts.map((post, idx) => {
-        const parsedDate = dayjs(post.created_at).format("MMM DD. YYYY");
+        const parsedDate = formatDate(post.created_at);
 
         return (
           <Link to={`/posts/${post.slug}`} className="post" key={idx}>
--- apps/client/app/components/Post/index.tsx
+++ apps/client/app/components/Post/index.tsx
@@ -3,6 +3,7 @@ import stylesUrl from "./styles.css";
 import prismStylesTheme from "prismjs/themes/prism-tomorrow.min.css";
 import markdownStylesUrl from "./markdown.css";
 import type { LinksFunction } from "@remix-run/node";
+import { formatDate } from "~/utils/date";
 
 export const links: LinksFunction = () => {
   return [
@@ -15,12 +16,14 @@ export const links: LinksFunction = () => {
 export interface Props {
   title: string;
   html: string;
+  date: string;
 }
 
-export const Post: FC<Props> = ({ title, html }) => {
+export const Post: FC<Props> = ({ title, html, date }) => {
   return (
     <div className="wrapper">
       <h2>{title}</h2>
+      <div className="date">{formatDate(date)}</div>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );

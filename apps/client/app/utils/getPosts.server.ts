eaeefb917d3afe655728c7ee16911ab75f946707
Walter <walter@sentbe.com>
2025-01-13 16:06:33 +0900

    인용 스타일 추가 / dev환경에서는 draft 포스트도 표시하도록 변경

--- apps/client/app/utils/getPosts.server.ts
+++ apps/client/app/utils/getPosts.server.ts
@@ -2,11 +2,15 @@ import { json } from "@remix-run/node";
 import { supabase } from "~/utils/supabaseClient.server";
 
 export async function getPosts() {
-  const { data, error } = await supabase
+  const includeDraft = process.env.NODE_ENV === "development";
+  const query = supabase
     .from("posts")
     .select("title,created_at,slug")
     .order("created_at", { ascending: false })
-    .eq("draft", false);
+
+  const { data, error } = includeDraft
+    ? await query
+    : await query.eq("draft", false);
 
   if (error) {
     throw new Error(error.message);
